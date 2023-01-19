4e6a2a480dc3e14e9c844b85ed432dd8052cb474
alishkeir <alishkeir_@hotmail.com>
2023-01-19 17:31:46 +0200

    Added command parsing using yargs

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log(process.argv);
+const yargs = require('yargs');
+
+// Cerate add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log('Adding a new note');
+    },
+});
+
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log('Removing a note');
+    },
+});
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: () => {
+        console.log('Listing all notes');
+    },
+});
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => {
+        console.log('Reading a note');
+    },
+});
+
+console.log(yargs.argv);
